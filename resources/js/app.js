f45fa17d2f1bc4ffba0baf57ee4debc49fa7d5d3
nikolaykislov <nikolaykislov888@gmail.com>
2023-11-10 20:08:20 +0300

    initial: repo prepare finish

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,10 +1,10 @@
-import Vue from 'vue'
+import Vue from "vue";
+import axios from "axios";
 
 //Main pages
-import App from './views/app.vue'
-
-
+import App from "./views/app.vue";
+Vue.prototype.$axios = axios;
 const app = new Vue({
-    el: '#app',
-    components: { App }
+    el: "#app",
+    components: { App },
 });
--- resources/js/views/app.vue
+++ resources/js/views/app.vue
@@ -1,18 +1,34 @@
 <template>
-    <div>
-        {{message}}
-    </div>
+    <div>Тестовое задание Кислов Николай.</div>
 </template>
 <script>
 const default_layout = "default";
-
 
 export default {
     computed: {},
     data() {
         return {
-            message:'Hello World'
-        }
-    }
+            message: "Hello 123",
+            data: [],
+        };
+    },
+
+    async created() {
+        await this.getPosts();
+    },
+
+    methods: {
+        async getPosts() {
+            console.log("here");
+            await this.$axios
+                .get(`/api/comments/`)
+                .then((res) => {
+                    this.data = res.data;
+                })
+                .catch((e) => {
+                    console.warn(e.message);
+                });
+        },
+    },
 };
 </script>
